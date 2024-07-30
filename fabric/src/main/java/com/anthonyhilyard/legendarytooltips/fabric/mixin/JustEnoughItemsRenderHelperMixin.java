ea43635fa6921369f21d2708aa66a47da2562cef
Anthony Hilyard <anthony.hilyard@gmail.com>
2024-07-30 11:53:32 -0700

    Updated to support TooltipAccess helper.

--- fabric/src/main/java/com/anthonyhilyard/legendarytooltips/fabric/mixin/JustEnoughItemsRenderHelperMixin.java
+++ fabric/src/main/java/com/anthonyhilyard/legendarytooltips/fabric/mixin/JustEnoughItemsRenderHelperMixin.java
@@ -1,7 +1,5 @@
 package com.anthonyhilyard.legendarytooltips.fabric.mixin;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -10,17 +8,15 @@ import java.util.stream.Stream;
 
 import mezz.jei.fabric.platform.RenderHelper;
 
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.Surrogate;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.anthonyhilyard.iceberg.util.ITooltipAccess;
 import com.anthonyhilyard.iceberg.util.Tooltips;
-import com.anthonyhilyard.legendarytooltips.LegendaryTooltips;
 import com.mojang.datafixers.util.Either;
 
 import net.minecraft.client.Minecraft;
@@ -36,9 +32,6 @@ import net.minecraft.world.item.ItemStack;
 @Mixin(RenderHelper.class)
 public class JustEnoughItemsRenderHelperMixin
 {
-	@Unique
-	Method setTooltipStack = null;
-
 	// For JEI versions prior to 19.5.
 	@Surrogate
 	private void setTooltipStack(Screen screen,
@@ -49,23 +42,7 @@ public class JustEnoughItemsRenderHelperMixin
 		Font font,
 		ItemStack itemStack, CallbackInfo info)
 	{
-		if (setTooltipStack == null)
-		{
-			try
-			{
-				setTooltipStack = GuiGraphics.class.getDeclaredMethod("setTooltipStack", ItemStack.class);
-			}
-			catch (Exception e)
-			{
-				LegendaryTooltips.LOGGER.error(ExceptionUtils.getStackTrace(e));
-			}
-		}
-
-		if (setTooltipStack != null)
-		{
-			try { setTooltipStack.invoke(graphics, itemStack); }
-			catch (IllegalAccessException | InvocationTargetException e) {}
-		}
+		((ITooltipAccess)graphics).setIcebergTooltipStack(itemStack);
 	}
 
 	@Inject(method = "renderTooltip(Lnet/minecraft/client/gui/GuiGraphics;Ljava/util/List;IILnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;)V",
@@ -77,20 +54,7 @@ public class JustEnoughItemsRenderHelperMixin
 		ItemStack itemStack,
 		CallbackInfo info)
 	{
-		if (setTooltipStack == null)
-		{
-			try
-			{
-				setTooltipStack = GuiGraphics.class.getDeclaredMethod("setTooltipStack", ItemStack.class);
-			}
-			catch (Exception e)
-			{
-				LegendaryTooltips.LOGGER.error(ExceptionUtils.getStackTrace(e));
-			}
-		}
-
-		try { setTooltipStack.invoke(graphics, itemStack); }
-		catch (IllegalAccessException | InvocationTargetException e) {}
+		((ITooltipAccess)graphics).setIcebergTooltipStack(itemStack);
 	}
 
 	@Redirect(method = "renderTooltip(Lnet/minecraft/client/gui/GuiGraphics;Ljava/util/List;IILnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;)V",
--- fabric/src/main/java/com/anthonyhilyard/legendarytooltips/fabric/mixin/RoughlyEnoughItemsScreenOverlayImplFabricMixin.java
+++ fabric/src/main/java/com/anthonyhilyard/legendarytooltips/fabric/mixin/RoughlyEnoughItemsScreenOverlayImplFabricMixin.java
@@ -1,15 +1,11 @@
 package com.anthonyhilyard.legendarytooltips.fabric.mixin;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.List;
 
+import com.anthonyhilyard.iceberg.util.ITooltipAccess;
 import com.anthonyhilyard.iceberg.util.Tooltips;
-import com.anthonyhilyard.legendarytooltips.LegendaryTooltips;
 
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At.Shift;
@@ -29,33 +25,14 @@ import net.minecraft.world.item.ItemStack;
 @Mixin(ScreenOverlayImplFabric.class)
 public class RoughlyEnoughItemsScreenOverlayImplFabricMixin
 {
-	@Unique
-	Method setTooltipStack = null;
-
 	@Inject(method = "renderTooltipInner(Lnet/minecraft/client/gui/screens/Screen;Lnet/minecraft/client/gui/GuiGraphics;Lme/shedaniel/rei/api/client/gui/widgets/Tooltip;II)V",
 			at = @At(value = "HEAD"), require = 0)
 	private void addItemStackAccess(Screen screen, GuiGraphics graphics, Tooltip tooltip, int mouseX, int mouseY, CallbackInfo info)
 	{
-		if (setTooltipStack == null)
-		{
-			try
-			{
-				setTooltipStack = GuiGraphics.class.getDeclaredMethod("setTooltipStack", ItemStack.class);
-			}
-			catch (Exception e)
-			{
-				LegendaryTooltips.LOGGER.error(ExceptionUtils.getStackTrace(e));
-			}
-		}
+		EntryStack<?> entryStack = tooltip.getContextStack();
+		ItemStack itemStack = entryStack.getType() == VanillaEntryTypes.ITEM ? entryStack.castValue() : ItemStack.EMPTY;
 
-		if (setTooltipStack != null)
-		{
-			EntryStack<?> entryStack = tooltip.getContextStack();
-			ItemStack itemStack = entryStack.getType() == VanillaEntryTypes.ITEM ? entryStack.castValue() : ItemStack.EMPTY;
-
-			try { setTooltipStack.invoke(graphics, itemStack); }
-			catch (IllegalAccessException | InvocationTargetException e) {}
-		}
+		((ITooltipAccess)graphics).setIcebergTooltipStack(itemStack);
 	}
 
 	@Inject(method = "renderTooltipInner(Lnet/minecraft/client/gui/screens/Screen;Lnet/minecraft/client/gui/GuiGraphics;Lme/shedaniel/rei/api/client/gui/widgets/Tooltip;II)V",
